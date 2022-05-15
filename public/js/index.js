7698adff31ffe0503660437026a4b4b48c5cee92
JakobGospoaric1 <jg9837@student.uni-lj.si>
2022-05-15 10:39:53 +0200

    Obarvane točke

--- public/js/index.js
+++ public/js/index.js
@@ -9,6 +9,26 @@ var points = 0;
 
 var coor = {};
 
+////////////////////////////
+var greenIcon = new L.Icon({
+  iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
+
+var redIcon = new L.Icon({
+  iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
+////////////////////////////
+
 
 
 navigator.geolocation.getCurrentPosition(createMap);
@@ -29,7 +49,10 @@ function createMap(position) {
         return result.json();
     }).then(result => {
         for (var i = 0; i < result.length; i++) {
-            L.marker({lat: result[i].lat, lng: result[i].lng}).addTo(map);
+            if(result[i].status)
+                L.marker({lat: result[i].lat, lng: result[i].lng}, {icon: redIcon}).addTo(map);
+            else
+            L.marker({lat: result[i].lat, lng: result[i].lng}, {icon: greenIcon}).addTo(map);
         }
     })
 }
